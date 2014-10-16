e2bd6c314bf596dc93db0af02067e14b9b02cafd
Magomogo <mgnatenko@gmail.com>
2014-10-16 11:47:27 +0200

    made Twitter component testable

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
     'use strict';
 
     var request = require('request'),
+        fs = require('fs'),
+        through = require('through2'),
+        NodeTwitter = require('node-twitter'),
 
         GooglePlus = require('./lib/GooglePlus.js'),
         Twitter = require('./lib/Twitter.js');
@@ -13,10 +16,19 @@
         },
 
         dest: {
-            twitter: new Twitter(request).writeStream
+            twitter: new Twitter(NodeTwitter.RestClient).writeStream,
+            publishedDate: function (filePath) {
+                return through.obj(function (doc, encoding, callback) {
+                    fs.writeFile(filePath, doc.published.toJSON(), callback);
+                });
+            }
         },
 
-        logger: require('./lib/streamLogger.js')
+        logger: require('./lib/streamLogger.js'),
+
+        filter: {
+            publishedAfter: require('./lib/publishedDateFilter.js')
+        }
     };
 
 }());
--- tests/Twitter.test.js
+++ tests/Twitter.test.js
@@ -1,8 +1,39 @@
 describe('Twitter', function () {
     'use strict';
 
+    var assert = require('assert'),
+        sinon = require('sinon'),
+        Twitter = require(__dirname + '/../lib/Twitter.js'),
+        testStreams = require(__dirname + '/../lib/test/streams.js');
+
     describe('writeStream', function () {
-        it('passes rePost documents through');
+        it('passes rePost documents through', function (done) {
+            var documentsPassedThrough = 0,
+                someConsumer = testStreams.writable(
+                    function (doc, encoding, callback) {
+                        documentsPassedThrough += 1;
+                        callback();
+                    },
+                    {objectMode: true}
+                ),
+                twitterClient = function () {
+                    var stub = sinon.stub();
+                    stub.yields(null);
+                    return {
+                        statusesUpdate: stub
+                    };
+                };
+
+            someConsumer.on('finish', function () {
+                assert.strictEqual(3, documentsPassedThrough);
+                done();
+            });
+
+            testStreams.rePostDocuments()
+                .pipe(new Twitter(twitterClient).writeStream({}))
+                .pipe(someConsumer);
+
+        });
     });
 
 });

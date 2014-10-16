fcad699ce905ae0bb700946994b4a55adebaa655
Magomogo <mgnatenko@gmail.com>
2014-10-16 11:46:08 +0200

    log dates differently

--- tests/streamLogger.test.js
+++ tests/streamLogger.test.js
@@ -3,26 +3,7 @@ describe('streamLogger', function () {
 
     var assert = require('assert'),
         logger = require(__dirname + '/../lib/streamLogger.js'),
-        testStreams = require(__dirname + '/../lib/test/streams.js'),
-
-        doc = function (timestamp, title) {
-            return {
-                published: timestamp.toUTCString(),
-                title: title
-            };
-        },
-
-        documentsStream = function () {
-            return testStreams.readable(
-                function () {
-                    this.push(doc(new Date('2014-01-01'), 'Happy new year'));
-                    this.push(doc(new Date('2014-03-08'), 'Happy woman\'s day'));
-                    this.push(doc(new Date('2014-05-09'), 'Happy victory day'));
-                    this.push(null);
-                },
-                {objectMode: true}
-            );
-        };
+        testStreams = require(__dirname + '/../lib/test/streams.js');
 
     it('passes through all incoming documents', function (done) {
         var documentsPassedThrough = 0,
@@ -39,7 +20,7 @@ describe('streamLogger', function () {
             done();
         });
 
-        documentsStream()
+        testStreams.rePostDocuments()
             .pipe(logger(testStreams.devNull()))
             .pipe(someConsumer);
     });
@@ -53,16 +34,16 @@ describe('streamLogger', function () {
 
         logDestinationStream.on('finish', function () {
             assert.equal(
-                'Wed, 01 Jan 2014 00:00:00 GMT: Happy new year\n' +
-                'Sat, 08 Mar 2014 00:00:00 GMT: Happy woman\'s day\n' +
-                'Fri, 09 May 2014 00:00:00 GMT: Happy victory day\n',
+                '2014-01-01T10:11:03.000Z: Happy new year\n' +
+                '2014-03-08T11:22:33.000Z: Happy woman\'s day\n' +
+                '2014-05-09T15:32:43.000Z: Happy victory day\n',
                 log
             );
 
             done();
         });
 
-        documentsStream().pipe(logger(logDestinationStream));
+        testStreams.rePostDocuments().pipe(logger(logDestinationStream));
     });
 
 });
